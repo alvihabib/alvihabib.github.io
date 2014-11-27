2ce6953850d2fc5a33caac28e847010db66b5190
Alvi Habib <alvi.hab@gmail.com>
2014-11-27 03:33:03 -0500

    overhaul

---
+++ tools/Tangerine_700.font.js
@@ -0,0 +1,19 @@
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * Copyright (c) 2010 by Toshi Omagari. All rights reserved.
+ * 
+ * Trademark:
+ * Tangerine is a trademark of Toshi Omagari.
+ * 
+ * Description:
+ * Copyright (c) 2010 by Toshi Omagari. All rights reserved.
+ * 
+ * Manufacturer:
+ * Toshi Omagari
+ * 
+ * Designer:
+ * Toshi Omagari
+ */
+Cufon.registerFont({"w":62,"face":{"font-family":"Tangerine","font-weight":700,"font-stretch":"normal","units-per-em":"360","panose-1":"2 0 0 0 0 0 0 0 0 0","ascent":"270","descent":"-90","bbox":"-106.767 -261.012 195.096 87.3539","underline-thickness":"18","underline-position":"-18","unicode-range":"U+0066-U+006F"},"glyphs":{" ":{},"f":{"d":"36,-84r-25,4v-1,-7,16,-11,27,-10v23,-68,50,-127,100,-162v26,-19,69,-7,54,27v-2,1,-4,1,-4,-1v-3,-23,-50,-23,-65,-9v-35,34,-47,89,-66,144v12,1,30,-3,38,2v-8,5,-27,1,-40,3v-27,80,-50,160,-137,173v-24,4,-33,-27,-16,-38v-6,26,33,33,54,21v48,-27,58,-89,80,-154"},"o":{"d":"11,-26v2,-34,21,-63,53,-66v16,-2,25,10,25,25v-2,35,-22,63,-53,67v-16,2,-26,-10,-25,-26xm54,-80v-27,0,-39,64,-9,69v17,-1,27,-21,26,-44v0,-13,-4,-25,-17,-25","w":92},"\u00a0":{}}});
